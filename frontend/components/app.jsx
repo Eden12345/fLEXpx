98c9d6185839ed2f8b92f5a28a72e6986f859019
Eden <ethangooding@gmail.com>
2018-01-02 18:22:49 -0500

    Fix AuthRoute import in App component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,14 +3,16 @@ import NavBarContainer from './nav_bar/nav_bar_container';
 import { Route, Switch } from 'react-router-dom';
 import LoginContainer from './session/login_container';
 import SignUpContainer from './session/sign_up_container';
+import { AuthRoute } from '../util/route_util';
+
 
 const App = () => (
   <div>
     <NavBarContainer />
     <h1>fLEXpx</h1>
     <Switch>
-      <Route path='/login' component={LoginContainer}/>
-      <Route path='/signup' component={SignUpContainer}/>
+      <AuthRoute path="/login" component={LoginContainer} />
+      <AuthRoute path="/signup" component={SignUpContainer} />
     </Switch>
   </div>
 );
