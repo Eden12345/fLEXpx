051eb6eb61919c9138b8cbfe1de87cd3eebfedbf
Eden <ethangooding@gmail.com>
2018-01-03 09:10:55 -0500

    Fix session actions so that error handling works properly

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -14,27 +14,29 @@ export const receiveErrors = errors => ({
 });
 
 //maybe will need to add .responseJSON to the errors arguments below
+//I did need to do that! It's so it goes into an array as a string rather
+//than a weird errors object (thanks Thomas)
 
 export const signup = user => dispatch => {
   return SessionAPIUtil.signup(user).then(responseUser => {
     return dispatch(receiveCurrentUser(responseUser));
   }, errors => {
-    return dispatch(receiveErrors(errors));
+    return dispatch(receiveErrors(errors.responseJSON));
   });
 };
 
 export const login = user => dispatch => {
   return SessionAPIUtil.login(user).then(responseUser => {
     return dispatch(receiveCurrentUser(responseUser));
-  }, err => {
-    return dispatch(receiveErrors(errors));
+  }, errors => {
+    return dispatch(receiveErrors(errors.responseJSON));
   });
 };
 
 export const logout = () => dispatch => {
   return SessionAPIUtil.logout().then(user => {
     return dispatch(receiveCurrentUser(null));
-  }, err => {
-    return dispatch(receiveErrors(errors));
+  }, errors => {
+    return dispatch(receiveErrors(errors.responseJSON));
   });
 };
