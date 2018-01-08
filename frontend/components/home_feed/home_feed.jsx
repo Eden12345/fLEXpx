657291ef7cb1fdeaff6854ed84dafb8abea76f27
Eden <ethangooding@gmail.com>
2018-01-08 17:48:00 -0500

    Add avatars and usernames to home feed items

--- frontend/components/home_feed/home_feed.jsx
+++ frontend/components/home_feed/home_feed.jsx
@@ -10,27 +10,33 @@ class HomeFeed extends React.Component {
   componentDidMount() {
     this.props.currentUser.followeeIds.forEach((userId) => {
       this.props.getPhotosForUser(userId);
+      this.props.getUser(userId);
     });
   }
   // debugger
 
   displayPhotos() {
     const that = this;
-    return this.props.currentUser.followeeUploads.map((userUploadsPair) => {
-      return (
-        <li key={Object.keys(userUploadsPair)}>
+    return that.props.currentUser.followeeUploads.map((userUploadsPair) => {
+      const userId = Object.keys(userUploadsPair);
+      if (that.props.users[userId]) {
+        return Object.values(userUploadsPair)[0].map((uploadId) => {
+          if (that.props.photos[uploadId]) {
+            const profilePhotoId = that.props.users[userId].profile_photo_id;
+            return (
+              <li key={that.props.photos[uploadId].id} className="home-feed-item">
+                <p>{that.props.users[userId].username}</p>
+                <img src={profilePhotoId ? that.props.photos[profilePhotoId].avatar : "https://s3.us-east-2.amazonaws.com/flexpx-dev/avatar.png"} />
+                <img src={that.props.photos[uploadId].medium} className="home-feed-photo"/>
+              </li>
+            );
+          }
+        });
+      } else {
+        return (<div></div>);
+      }
 
-          {Object.values(userUploadsPair)[0].map((uploadId) => {
-            if (that.props.photos[uploadId]) {
-              return (
-                  <img key={that.props.photos[uploadId].id}
-                    src={that.props.photos[uploadId].medium}/>
-              );
-            }
-          })}
-          
-        </li>
-      );
+
     });
   }
 
--- frontend/components/home_feed/home_feed_container.js
+++ frontend/components/home_feed/home_feed_container.js
@@ -2,14 +2,17 @@ import { connect } from 'react-redux';
 
 import HomeFeed from './home_feed';
 import { getPhotosForUser } from '../../actions/photo_actions';
+import { getUser } from '../../actions/user_actions';
 
 const mapStateToProps = ({ session, entities }) => ({
   currentUser: session.currentUser,
-  photos: entities.photos
+  photos: entities.photos,
+  users: entities.users
 });
 
 const mapDispatchToProps = dispatch => ({
-  getPhotosForUser: (userId) => dispatch(getPhotosForUser(userId))
+  getPhotosForUser: (userId) => dispatch(getPhotosForUser(userId)),
+  getUser: (userId) => dispatch(getUser(userId))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeFeed);
