520caf0c755c9a4087a7c1d133cc485425ebb81b
Eden <ethangooding@gmail.com>
2018-01-07 23:07:07 -0500

    Fix navbar to top of window and make profile drop-down a hover menu

--- frontend/components/nav_bar/profile-dropdown.jsx
+++ frontend/components/nav_bar/profile-dropdown.jsx
@@ -8,12 +8,13 @@ class ProfileDropdown extends React.Component {
       open: false
     };
     this.toggleMenu = this.toggleMenu.bind(this);
+    this.menuOn = this.menuOn.bind(this);
     this.logoutAndClose = this.logoutAndClose.bind(this);
     this.renderAvatar = this.renderAvatar.bind(this);
   }
 
   componentDidMount() {
-    const avatarId = this.props.currentUser.profile_photo_id;
+    // const avatarId = this.props.currentUser.profile_photo_id;
     // this.props.getPhoto(avatarId);
   }
 
@@ -24,6 +25,11 @@ class ProfileDropdown extends React.Component {
     } else {
       this.setState({open: false});
     }
+  }
+
+  menuOn(e) {
+    e.stopPropagation();
+    this.setState({open: true});
   }
 
   logoutAndClose(e) {
@@ -41,15 +47,13 @@ class ProfileDropdown extends React.Component {
       return (
         <img src={this.props.photos[avatarId].avatar}
           id="menu-button"
-          className="profile-image nav-bar-avatar"
-          onClick={this.toggleMenu}/>
+          className="profile-image nav-bar-avatar"/>
       );
     } else {
       return (
         <img src="https://s3.us-east-2.amazonaws.com/flexpx-dev/avatar.png"
           id="menu-button"
-          className="profile-image"
-          onClick={this.toggleMenu}/>
+          className="profile-image"/>
       );
     }
   }
@@ -57,11 +61,12 @@ class ProfileDropdown extends React.Component {
   render() {
     return (
       <nav className="profile-logout">
-        <button className="profile-dropdown-parent">
+        <button className="profile-dropdown-parent" onMouseEnter={this.menuOn} onMouseLeave={this.toggleMenu}>
           {this.renderAvatar()}
           <div className={this.state.open === true ? "profile-dropdown-content" : "profile-dropdown-hidden"}>
             <p className="profile-name">{this.props.currentUser.username}</p>
-            <Link to={`/profile/${this.props.currentUser.id}`} className="button dropdown-button profile-link">My profile</Link>
+            <Link to={`/profile/${this.props.currentUser.id}`}
+              className="button dropdown-button profile-link">My profile</Link>
             <div className="button dropdown-button" onClick={this.logoutAndClose}>Log out</div>
           </div>
         </button>
